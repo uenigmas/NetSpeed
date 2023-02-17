c130a46d8dd14e1fc32fd7869ffba0df8d5e6464
lpx <1504958548@qq.com>
2023-02-17 22:41:26 +0800

    添加CPU、内存计算

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
-#include <QDebug>
 #include <QApplication>
+#include <QDebug>
+#include <QFile>
 #include <QTimer>
 
 QString DEMO_CPU = "CPU:%1% MEM:%2%";
@@ -23,8 +24,7 @@ QString humanize(float netspeed)
 {
     static const char unit[] = {'B', 'K', 'M', 'G', 'T'};
     int n = 0;
-    while (netspeed >= 1000)
-    {
+    while (netspeed >= 1000) {
         netspeed /= 1024;
         n++;
     }
@@ -35,7 +35,33 @@ QString humanize(float netspeed)
 // ''' 获取cpu、内存占用数据 '''
 QString get_cpu_and_mem_date()
 {
-    return DEMO_CPU;
+    static const char *PROC_STAT = "/proc/stat";
+    static const char *PROC_MEM = "/proc/meminfo";
+
+    // CPU
+    QFile f(PROC_STAT);
+    if (!f.open(QIODevice::ReadOnly))
+        return "";
+    auto split = QString(f.readLine()).split(' ', Qt::SkipEmptyParts);
+    f.close();
+    split.removeAt(0);
+    auto sum = 0, idle = split[3].toInt();
+    for (auto e : split) {
+        sum += e.toInt();
+    }
+    float cpu = 1 - idle * 1.0 / sum;
+
+    // Memory
+    f.setFileName(PROC_MEM);
+    if (!f.open(QIODevice::ReadOnly))
+        return "";
+    auto memTotal = QString(f.readLine()).split(' ', Qt::SkipEmptyParts)[1].toInt();
+    f.readLine();
+    auto memAvailable = QString(f.readLine()).split(' ', Qt::SkipEmptyParts)[1].toInt();
+    f.close();
+    float mem = 1 - memAvailable * 1.0 / memTotal;
+
+    return DEMO_CPU.arg(cpu * 100, 0, 'g', 3).arg(mem * 100, 0, 'g', 3);
 }
 
 // ''' 获取网络数据 '''
@@ -83,8 +109,7 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
 
 void MainWindow::mouseReleaseEvent(QMouseEvent *event)
 {
-    if (event->button() == Qt::LeftButton)
-    {
+    if (event->button() == Qt::LeftButton) {
         leftClick = false;
         return;
     }
